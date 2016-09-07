a05d8abc9ebfc806ef505f67616274daffa5d0bc
Georg Fleig <georg.fleig@sophos.de>
2016-09-07 11:25:42 +0200

    NUTM-5099 Updates license and refactors CW role creation

--- lib/subcmd/ogw-create-cw-role.sh
+++ lib/subcmd/ogw-create-cw-role.sh
@@ -1,33 +1,58 @@
 # Copyright 2016 Sophos Technology GmbH. All rights reserved.
-# This software may be modified and distributed under the terms
-# of the MIT license. See the LICENSE file for details.
+# See the LICENSE.txt file for details.
 # Authors: Georg Fleig
 
 source $LIB_DIR/helper.subr
 
 with_temp_and_region
 
-CWROLE="EC2ActionsAccess"
+CW_ROLE="EC2ActionsAccess"
+POLICY_ARN="arn:aws:iam::aws:policy/CloudWatchActionsEC2Access"
+
+function create_role() {
+  aws iam create-role --role-name="$1" --path="/actions/" \
+                      --assume-role-policy-document='{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "swf.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }' 2>&1 >/dev/null
+}
+
+function delete_role() {
+  aws iam delete-role --role-name="$1" > /dev/null
+}
+
+function attach_policy_to_role() {
+  aws iam attach-role-policy --role-name="$1" \
+                             --policy-arn="$2" > /dev/null
+}
 
 # We do not use get-role first to check for the existence of the role as this
 # would require additional rights that we might not have.
-printf "Trying to create IAM role \"$CWROLE\"."
-ROLE_CREATE_ERR=$(aws iam create-role --role-name $CWROLE --path="/actions/" --assume-role-policy-document='{"Version":"2012-10-17","Statement":[{"Action":"sts:AssumeRole","Principal":{"Service":"swf.amazonaws.com"},"Effect":"Allow","Sid":""}]}' 2>&1 >/dev/null)
+printf "Trying to create IAM role \"$CW_ROLE\": "
+ROLE_CREATE_ERR=$(create_role $CW_ROLE)
 
 if [[ $ROLE_CREATE_ERR =~ "EntityAlreadyExists" ]]; then
-  printf "\nRole already exists, nothing to do here.\n"
+  printf "Role already exists, will not touch it.\n"
   exit 0
 elif [[ $ROLE_CREATE_ERR != "" ]]; then
   printf "$ROLE_CREATE_ERR\n"
   exit 2
 fi
 
-printf " Done\nAttaching policy."
-aws iam attach-role-policy --role-name $CWROLE --policy-arn="arn:aws:iam::aws:policy/CloudWatchActionsEC2Access" > /dev/null
-if [[ $? ]]; then
-  printf " Done\n"
-else
-  printf " Failed! Will delete the empty role.\n"
-  aws iam delete-role --role-name $CWROLE > /dev/null || exit 3
+printf "done\nAttaching policy: "
+if ! attach_policy_to_role $CW_ROLE $POLICY_ARN; then
+  printf "Failed! Will delete the empty role.\n"
+  delete_role $CW_ROLE || exit 3
   exit 4
 fi
+
+printf "done\n"
--- lib/subcmd/ogw-routes.sh
+++ lib/subcmd/ogw-routes.sh
@@ -1,6 +1,5 @@
 # Copyright 2016 Sophos Technology GmbH. All rights reserved.
-# This software may be modified and distributed under the terms
-# of the MIT license. See the LICENSE file for details.
+# See the LICENSE.txt file for details.
 # Authors: Vincent Landgraf
 
 source $LIB_DIR/helper.subr
